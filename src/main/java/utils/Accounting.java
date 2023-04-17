770953e4e5e6724499921c80066eb6e31e069f9a
tabinda-rmit <98718986+tabinda-rmit@users.noreply.github.com>
2023-04-17 15:47:05 +1000

    Add files via upload

---
+++ src/main/java/utils/Accounting.java
@@ -0,0 +1,31 @@
+package utils;
+
+import model.Student;
+import model.Department;
+import model.Course;
+
+/**
+ * Utility class
+ */
+public class Accounting {
+
+    /**
+     * Calculates fee for a student in enrolled courses.
+     * @param student: Student to calculate the fee for.
+     * @return Fee for a semester
+     */
+    public double calculateFee(Student student){
+        return 0d;
+    }
+
+    /**
+     * Calculates the costs of a course considering the start and end dates
+     * @param course: Course for which the cost is to be calculated
+     * @return cost of the course
+     */
+    public double courseCost(Course course){
+        return 0d;
+    }
+
+
+}
---
+++ src/main/java/utils/StudentAffairs.java
@@ -0,0 +1,21 @@
+package utils;
+
+import model.Course;
+import model.Student;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Utility class to perform HR operations
+ */
+public class StudentAffairs {
+
+    public List<Student> findStudentInCourse(Course course){
+        return null;
+    }
+
+    public List<Student> findStudentPassed(Student student){
+        return null;
+    }
+}
