0489423c8cacaa5ef16ad45565bde338460f9e08
tabinda-rmit <98718986+tabinda-rmit@users.noreply.github.com>
2023-04-17 15:48:18 +1000

    Add files via upload

---
+++ src/test/java/model/AddStudent.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Date;
+
+/**
+ *  Implement and test {Programme.addStudent } that respects the considtion given the assignment specification
+ * NOTE: You are expected to verify that the constraints to add a new student to a programme are met.
+ *
+ * Each test criteria must be in an independent test method .
+ *
+ * Initialize the test object with initialise method.
+ */
+public class AddStudent {
+	
+	
+
+
+}
